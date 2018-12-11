f676d38e74ace21fe7c5d22a93baa32842f2c841
ahmadnish <ahmad.neishabouri@gmail.com>
2018-12-11 03:11:50 +0100

    turn slabgeometry to a function and clean up

--- slabgeometry.m
+++ slabgeometry.m
@@ -1,18 +1,12 @@
-% function slabgeometry(gantryAngle, couchAngle)
-clc,clear, close all
+function [ct, cst, pln, stf, resultGUI] = slabgeometry(ct, cst, vars)
 
-load BOXPHANTOM_2mm.mat
-
-ct_org = ct;
-
-%%
 pln.radiationMode = 'protons';
 pln.machine = 'generic_TOPAS_cropped';
-% pln.machine = 'Generic'
-%%
-pln.numOfFractions        = 1;
-pln.propStf.gantryAngles  = 0;
-pln.propStf.couchAngles   = 0;
+
+% setting up the plan
+pln.numOfFractions        = 30;
+pln.propStf.gantryAngles  = vars.gantryAngle;
+pln.propStf.couchAngles   = vars.couchAngle;
 pln.propStf.bixelWidth    = 150;
 pln.propStf.longitudinalSpotSpacing = 150;
 pln.propStf.numOfBeams    = numel(pln.propStf.gantryAngles);
@@ -20,97 +14,46 @@ pln.propStf.isoCenter     = ones(pln.propStf.numOfBeams,1) * matRad_getIsoCenter
 pln.propOpt.runDAO        = 0;
 pln.propOpt.runSequencing = 0;
 
-cst{2,6}.dose = 146;
+% slab geometry and alignment
+geo = vars.geo;
+alignment = vars.alignment;
 
-% pln.bioParam = matRad_bioModel(pln.radiationMode, quantityOpt, modelName);
+% setting up dose with realistic numbers considering it's one bixel and
+% only one fraction
+cst{2,6}.dose = .2;
+cst{2,6}.dose = .5;
+
 pln.propOpt.bioOptimization = 'none';
-% pln.multScen = matRad_multScen(ct, 'nomScen');
-%% define slap geometry
-slice = round(pln.propStf.isoCenter(1,3)./ct.resolution.z);
-scs = 8;
 
-for ii = 4
-    slab_x = -2; % relevant to isocenter in x direction
-    slab_y = ii * -10; % relevant to isocenter in y direction
+% reading the isocenter
+slab_loc = pln.propStf.isoCenter; 
 
-    geo = [3 15 3]; % geometry of the slab
+% turning it into voxels and aligning it
+slab_loc(1) = floor(slab_loc(1)/ct.resolution.y) + alignment(1);
+slab_loc(2) = floor(slab_loc(2)/ct.resolution.x) + alignment(2);
+slab_loc(3) = floor(slab_loc(3)/ct.resolution.z) + alignment(3);
 
-    slab_dim(1) = (2*geo(1) + 1) * ct.resolution.y;
-    slab_dim(2) = (2*geo(2) + 1) * ct.resolution.x;
-    slab_dim(3) = (2*geo(3) + 1) * ct.resolution.z;
-
-    slab_loc = pln.propStf.isoCenter;
-
-    slab_loc(1) = slab_loc(1) + slab_y; % moving y dimension
-    slab_loc(2) = slab_loc(2) + slab_x;  % moving x dimension
-
+% bulding the mask for where the slab is
+mask = zeros(ct.cubeDim);
     
-    slab_loc(1) = floor(slab_loc(1)/ct.resolution.y);
-    slab_loc(2) = floor(slab_loc(2)/ct.resolution.x);
-    slab_loc(3) = floor(slab_loc(3)/ct.resolution.z);
-    
-    theta = pi;
-    
-    for jj = 0 % zero means no change
-        
-        mask = zeros(ct.cubeDim);
-        
-        tform = affine3d([1         0               0                0; ...
-                          0         cos(jj * theta) sin(jj * theta)  0; ...
-                          0         -sin(jj *theta) cos(jj * theta)  0; ...
-                          0         0               0                1]);
-                            
-                      
-        for i = -geo(1):geo(1)
-            for j = -2 * geo(2) : 0
-                for z = -geo(3):geo(3)
-                    ix = slab_loc + [i, j, z];
-                    mask(ix(1),ix(2),ix(3)) = 1;
-                end
-            end
+for i = -geo(1):geo(1)
+    for j = -2 * geo(2) : 0
+        for z = -geo(3):geo(3)
+            ix = slab_loc + [i, j, z];
+            mask(ix(1),ix(2),ix(3)) = 1;
         end
-
-%         mask = imwarp(mask, tform);
-%         subplot(1,2,jj+1)
-%         imagesc(mask(:,:,slice))
-%         pause(.2)
-%         
-        ct.cube{1}(mask == 1) = 2;
-        ct.cubeHU{1}(mask == 1) = 1024;
-%         ct.cube{1} = ct_org.cube{1};
-%         ct.cubeHU{1} = ct_org.cubeHU{1};
     end
-    %
-    stf = matRad_generateStf(ct, cst, pln);
-    stf.ray.energy = 116.3620;
-
-    dij = matRad_calcParticleDose(ct,stf,pln,cst);
-
-    resultGUI = matRad_fluenceOptimization(dij,cst,pln);
-
-    filename = ['topas_ws_',int2str(slab_loc(1)),'_',int2str(slab_loc(2)), ...
-        '_',int2str(slab_loc(3)),'_',int2str(geo(1)),'_',int2str(geo(2)), ...
-        '_',int2str(geo(3))];
-    save([filename, '.mat']);
-%     nish_Submit_MC_calculation_via_SSH(filename, 1e-3)
-%     
-    ct.cube{1} = ct_org.cube{1};
-    ct.cubeHU{1} = ct_org.cubeHU{1};
 end
 
-matRadGUI
+% assigning electron density to the slab
+ct.cube{1}(mask == 1) = vars.slab_ed;
+ct.cubeHU{1}(mask == 1) = vars.slab_hu;
 
-%         vis = ct.cube{1};
-%         vis (vis == 1) = 0;
-%         subplot(221)
-%         imagesc(vis(:,:,slice))
-%         title(['Z ii = ', num2str(ii), 'jj = ', num2str(jj)])
-%         subplot(222)
-%         imagesc(squeeze(vis(:,slice,:)))
-%         title(['Y ii = ', num2str(ii), 'jj = ', num2str(jj)])
-%         subplot(223)
-%         imagesc(squeeze(vis(slice,:,:)))
-%         title(['X ii = ', num2str(ii), 'jj = ', num2str(jj)])
-%         hold on
-%         pause(1)
-        
+stf = matRad_generateStf(ct, cst, pln);
+stf.ray.energy = vars.Energy;
+
+dij = matRad_calcParticleDose(ct,stf,pln,cst);
+
+resultGUI = matRad_fluenceOptimization(dij,cst,pln);
+
+end
