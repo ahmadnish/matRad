2cf6a12b564ed5b629f65dc005ab05bf3cad7501
ahmadnish <neishabouri@stud.uni-heidelberg.de>
2018-12-12 19:55:08 +0100

    added the Ellipsoid geometry to slabGeometry - Pyramid not completed yet

--- slabGeometry.m
+++ slabGeometry.m
@@ -14,13 +14,39 @@ mask = zeros(cubeDim);
 
 switch shape
     
-    case 'Rec'
+    case 'Rectangle'
         
         for i = -geo(1):geo(1)
-            for j = -2 * geo(2) : 0
+            for j = -geo(2) : geo(2)
                 for z = -geo(3):geo(3)
                     ix = slab_loc + [i, j, z];
                     mask(ix(1),ix(2),ix(3)) = 1;
+                end
+            end
+        end
+        
+    case 'Ellipsoid'
+        
+        for i = -geo(1):geo(1)
+            for j = -geo(2):geo(2)
+                for z = -geo(3):geo(3)
+                    ix = slab_loc + [i,j,z];
+                    if (sum(([i,j,z].^2) ./ (geo.^2)) <= 1)
+                        mask(ix(1),ix(2),ix(3)) = 1;
+                    end                       
+                end
+            end
+        end
+
+    case 'Pyramid'
+        
+        for i = 0:geo(1)
+            for j = 0:geo(2)
+                for z = 0:geo(3)
+                    ix = slab_loc + [i,j,z];
+                    if (i <= (floor(1 - j/geo(2)) * geo(1)) && z <= (floor(1 - j/geo(2)) * geo(3)))
+                        mask(ix(1),ix(2),ix(3)) = 1;
+                    end                       
                 end
             end
         end
@@ -31,6 +57,8 @@ switch shape
             
 end
 
-
-            
+% 
+%     center = slab_loc - [0 round(geo(2)/2) 0];
+%                     currPos = center + [i j z];
+                             
 end
