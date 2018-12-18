8a943fa143b27432429bbf2d0a5ef1bf0cbb01bc
ahmadnish <neishabouri@stud.uni-heidelberg.de>
2018-12-18 12:39:13 +0100

    adding the range of change for geometry size in an adapted way comparing to the peak position and alignment

--- ts_factory.m
+++ ts_factory.m
@@ -5,6 +5,8 @@ clc,clear ,tic
 %% Write down all the possible values for the parameters
 load protons_generic_TOPAS_cropped.mat
 
+finalCubeSize = [80 28 28]; % the extraction cube for ANN
+
 particleEnergies = [machine.data.energy];
 peakPos = [machine.data.peakPos];
 
@@ -13,10 +15,20 @@ peakPos = peakPos(7:28);
 
 slabSPs = 0.00324:.01:2.53061; %253 cases
 
+slabXs = 1:finalCubeSize(1)/2; % 41 cases
+slabYs = 1:finalCubeSize(2)/2; % 15 cases
+slabZs = finalCubeSize(3)/2;   % fixed value
+
+alignmentsX = -40:0; % 41 cases
+
 slab_goe_xs = 0;
 slabGeometeries = ["Rectangle", "Ellipsoid", "Pyramid", "2DPyramid"];
-numOfSamples = 1;
+numOfSamples = 10;
 %% Random sample from the possible values
+boxSize = 160 * ones(1,3);
+res = 2 * ones(1,3);
+
+
 vars = struct;
 i = 1;
 tmp2 = 0;
@@ -25,12 +37,21 @@ while (i <= numOfSamples && tmp2 < 10)
     vars(i).gantryAngle = 0;
     vars(i).couchAngle = 0;
     vars(i).geoShape = '2DPyramid';
-    vars(i).geoSize = [14 16 14]; % size of the geometry in voxels
-                                  % for Rectagle:
-                                  % for Triangle:
-                                  % for Circle:
+   
+    vars(i).Energy = randsample(particleEnergies, 1); % desired energy for the particle
+    peakPosition = peakPos(vars(i).Energy == particleEnergies);
+    peakPosition = peakPosition/res(3);
     
-    vars(i).alignment = [-30 -16 0]; % aligns the center point in respect to the
+%     vars(i).geoSize = [40 14 14];
+    vars(i).geoSize = zeros(1,3);
+    vars(i).geoSize(1) = randsample(slabXs(slabXs < peakPosition/2),1);
+    vars(i).geoSize(2) = randsample(slabYs,1);
+    vars(i).geoSize(3) = randsample(slabZs,1); % 
+%     
+    vars(i).alignment = [0 -15 0]; 
+    vars(i).alignment(1) = randsample(alignmentsX,1);
+    
+    % aligns the center point in respect to the
     % isocneter which in turn will be the starting point
     % for building the geometry. y and z are build around
     % this point while x is build toward left or right
@@ -40,7 +61,6 @@ while (i <= numOfSamples && tmp2 < 10)
     
     vars(i).slab_sp = randsample(slabSPs,1); % slab's stopping power
     
-    vars(i).Energy = randsample(particleEnergies, 1); % desired energy for the particle
     
     tmp1 = 1;
     for ii = 1:i-1
@@ -57,18 +77,16 @@ while (i <= numOfSamples && tmp2 < 10)
     
 end
 
-clearvars -except vars numOfSamples
-
-boxSize = 160 * ones(1,3);
-res = 2 * ones(1,3);
 [ct, cst] = makeBoxphantom(boxSize, res);
 
 for i = 1:numOfSamples
     [ct, cst, pln, stf, resultGUI, mask] = doseCalc(ct, cst, vars(i));
     close
-    filename = ['topas_ws_', num2str(i, '%.6u'), '.mat'];
-    
-    save(filename, 'ct', 'cst', 'pln', 'resultGUI', 'stf');
+    matRadGUI
+    pause(5)
+%     filename = ['topas_ws_', num2str(i, '%.6u'), '.mat'];
+%     
+%     save(filename, 'ct', 'cst', 'pln', 'resultGUI', 'stf');
 end
 
 toc
