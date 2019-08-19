d8f4242f8941336974e65c1cfe878e957956f550
ahmadnish <neishabouri@stud.uni-heidelberg.de>
2019-08-19 12:02:20 +0200

    gammaIndexPlotter code update, making it show dose distributions in vertical

--- gammaIndexPlotter.m
+++ gammaIndexPlotter.m
@@ -16,7 +16,7 @@ dose = dose * 1.992;
 dose_ann = dose_ann/1000;
 dose_ann = dose_ann * 1.992;
 
-[gammaCube] = matRad_gammaIndex(dose,dose_ann,[2 2 2], [1 2], [], 3);
+[gammaCube] = matRad_gammaIndex(dose,dose_ann,[2 2 2], criteria, [], 3);
 
 pass = 100 - ((nnz(gammaCube>1) * 100 )/(nnz(gammaCube>0)));
 
@@ -25,7 +25,7 @@ if plotting
 
     
     figure
-    set(gcf, 'Units', 'Normalized', 'OuterPosition', [0, 0.04, .5, 0.96]);
+    set(gcf, 'Units', 'Normalized', 'OuterPosition', [0, 0.3, .8, .7]);
     
     combined = [dose dose_ann];
     mAx = max(combined(:));
@@ -37,30 +37,30 @@ if plotting
     
 
         for i = slices
-            ax = subplot(411);
-            imagesc(squeeze(ct(:,:,i))')
+            ax = subplot(141);
+            imagesc(squeeze(ct(:,:,i)))
             title('CT input (RSP)')
             colorbar()
             colormap(gca, 'gray')
             caxis(ax, [0, 2.5])
 
-            ax1 = subplot(412);
-            imagesc(squeeze(dose(:,:,i))')
+            ax1 = subplot(142);
+            imagesc(squeeze(dose(:,:,i)))
             colorbar()
             colormap(gca, 'jet')
-            title(['Monte Carlo Simulation [Gy] - Integral Dose = ', num2str(sum(dose(:)), '%.4f'), ' Gy'])
-            ax2 = subplot(413);
-            imagesc(squeeze(dose_ann(:,:,i))')
-            title(['Estimated dose by ANN [Gy] - Integral Dose = ', num2str(sum(dose_ann(:)), '%.4f'), ' Gy'])
+            title({'Monte Carlo Simulation [Gy]', ['Integral Dose = ', num2str(sum(dose(:)), '%.4f'), ' Gy']})
+            ax2 = subplot(143);
+            imagesc(squeeze(dose_ann(:,:,i)))
+            title({'Estimated dose by ANN [Gy]', ['Integral Dose = ', num2str(sum(dose_ann(:)), '%.4f'), ' Gy']})
             colorbar()
             colormap(gca, 'jet')
 
             caxis(ax1, [mIN, mAx])
             caxis(ax2, [mIN, mAx])
 
-            subplot(414)
-            imagesc(squeeze(gammaCube(:, :, i))')
-            title(['Gamma index analysis [\gamma] - Pass rate = ', num2str(pass) ' %'])
+            subplot(144)
+            imagesc(squeeze(gammaCube(:, :, i)))
+            title({'Gamma index analysis [\gamma]', ['Pass rate = ', num2str(pass) ' %']})
             caxis([0 2]), colormap(gca, matRad_getColormap('gammaIndex')), colorbar
 
             pause(1)
